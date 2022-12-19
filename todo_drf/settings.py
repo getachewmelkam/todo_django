3a2433ae27a62d92ea42eea58ea5c66023d789a4
getachewmelkam <gechabyssinia12@gmail.com>
2022-12-19 12:47:11 +0300

    django project

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -83,10 +83,10 @@ WSGI_APPLICATION = 'todo_drf.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
@@ -127,9 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'frontend/build/static')
 ]
 CORS_ALLOWED_ORIGINS = ['.vercel.app', '.now.sh']
-STATICFILES_DIRS = [BASE_DIR/'static',]
-STATIC_ROOT = BASE_DIR/'staticfiles'
+
